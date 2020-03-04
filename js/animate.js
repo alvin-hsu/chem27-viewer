bb66342f6906695207834b97634c67259635100a
Alvin Hsu <alvin.hsu@berkeley.edu>
2020-03-04 12:51:33 -0500

    Animated NADH reduction

---
+++ js/animate.js
@@ -0,0 +1,49 @@
+// stage is stage
+// forward is forward pdb file,
+// reverse is reverse pdb file,
+// ts is frame # for transition state (change between f/r)
+function animate(stage, forward, reverse, ts) {
+    stage.loadFile(forward, {asTrajectory: true}).then (function (o_f) {
+    stage.loadFile(reverse, {asTrajectory: true}).then (function (o_r) {
+        var traj_f = o_f.addTrajectory().trajectory;
+        var traj_r = o_r.addTrajectory().trajectory;
+        var player_f = new NGL.TrajectoryPlayer(traj_f, {
+            step: 1,
+            timeout: 1,
+            interpolateStep: 1,
+            start: 0,
+            end: ts,
+            interpolateType: 'linear',
+            mode: 'once',
+            direction: 'forward'
+        });
+        var player_r = new NGL.TrajectoryPlayer(traj_r, {
+            step: 1,
+            timeout: 1,
+            interpolateStep: 1,
+            start: 0,
+            end: traj_r.frameCount - ts,
+            interpolateType: 'linear',
+            mode: 'once',
+            direction: 'backward'
+        });
+
+        var repr;
+
+        player_f.signals.haltedRunning.add(function (e) {
+            o_f.removeRepresentation(repr);
+            repr = o_r.addRepresentation('ball+stick');
+            player_r.play();
+        });
+        player_r.signals.haltedRunning.add(function (e) {
+            o_r.removeRepresentation(repr);
+            repr = o_f.addRepresentation('ball+stick');
+            player_f.play();
+        });
+
+        repr = o_f.addRepresentation('ball+stick');
+        o_f.autoView();
+        player_f.play();
+    });
+    });
+}
